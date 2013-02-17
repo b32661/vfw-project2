bcb74859df929869fcb4a943dd2921ed0554980f
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-02-17 02:44:38 -0500

    javascript variables

--- js/main.js
+++ js/main.js
@@ -4,36 +4,14 @@
  *   Project 2
  */  
 
-var purposeBox = document.getElementById("sstyle");
-
-
- 
+var purposeBox = document.getElementById("sstyle"); 
 var sureSlider = document.getElementById("ssure");
-
-
-
 var preferFormat = document.getElementsByName("timeformat");
-
-
- 
 var eventName = document.getElementById("sname");
-
-
-
-
- 
 var eventDetails = document.getElementById("sdescription");
-var changeBorder = function (){
-	eventDetails.setAttribute("class", "hasFocus");
-}
-eventDetails.addEventListener("focus", changeBorder);
-
-
- 
 var eventDate = document.getElementById("sdate");
-
-
- 
 var eventTime = document.getElementById("stime");
 
 
+console.log("sstyle = " + purposeBox + "sureSlider = " + sureSlider + "timeformat = " + preferFormat +
+	"sname = " + eventName + "sdescription = " + eventDetails + "sdate = " + eventDate + "stime = " +eventTime);
