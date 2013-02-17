74ff4a49fbf0b8085e78d92764883e677da31124
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-02-17 07:55:23 -0500

    bunch of functions

--- js/main.js
+++ js/main.js
@@ -3,15 +3,66 @@
  *   VFW 1302
  *   Project 2
  */  
+ 
+ 
+ // declare variables
+var myEvent = document.getElementById("myEvent");
+var eventName = document.getElementById("eventName");
+var eventDescription = document.getElementById("eventDescription");
+var eventDate = document.getElementById("eventDate");
+var eventTime = document.getElementById("eventTime");
+var sure = document.getElementById("sure");
+var clearData = document.getElementById("clear");
+var preferredFormat = document.getElementById("stomper").timeFormat;
+var send = document.getElementById("stomp");
+var sureSpan = sure.getAttribute("span");
 
-var purposeBox = document.getElementById("sstyle"); 
-var sureSlider = document.getElementById("ssure");
-var preferFormat = document.getElementsByName("timeformat");
-var eventName = document.getElementById("sname");
-var eventDetails = document.getElementById("sdescription");
-var eventDate = document.getElementById("sdate");
-var eventTime = document.getElementById("stime");
+
+var captureName = function (){
+	localStorage.setItem("name", eventName.value);
+};
 
 
-console.log("sstyle = " + purposeBox + "sureSlider = " + sureSlider + "timeformat = " + preferFormat +
-	"sname = " + eventName + "sdescription = " + eventDetails + "sdate = " + eventDate + "stime = " +eventTime);
+var captureDescription = function(){
+	localStorage.setItem("description", eventDescription.value);
+};
+
+
+var captureDate = function(){
+	localStorage.setItem("date", eventDate.value);
+};
+
+var captureTime = function(){
+	localStorage.setItem("time", eventTime.value);
+};
+
+
+var captureSure = function(){
+	localStorage.setItem("sure strength", sure.value);
+	
+};
+
+var captureMyEvent = function(){
+	localStorage.setItem("event", myEvent.value);
+};
+
+var clearStorage = function(){
+	localStorage.clear();
+};
+
+
+var sendForm = function(){
+	
+};
+
+//event listeners
+eventName.addEventListener("blur", captureName);
+eventDescription.addEventListener("blur", captureDescription);
+eventDate.addEventListener("blur", captureDate);
+eventTime.addEventListener("blur", captureTime);
+clearData.addEventListener("click", clearStorage);
+send.addEventListener("click", sendForm);
+sure.addEventListener("change", captureSure);
+myEvent.addEventListener("change", captureMyEvent);
+
+console.log(sureSpan);
